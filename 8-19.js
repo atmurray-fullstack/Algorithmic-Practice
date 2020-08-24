3a63a2502bdee54cbdb817f80b7e75aa7010d9f2
Andrew Murray <atmurray@bellsouth.net>
2020-08-23 23:24:11 -0400

    saving more algo practice solutions

--- 8-19.js
+++ 8-19.js
@@ -1,9 +1,24 @@
-// In this kata you have to create all permutations of an input string and remove
-// duplicates, if present. This means, you have to shuffle all letters from the input 
-// in all possible orders.
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.For example:
 
-// Examples:
+// domainName("http://github.com/carbonfive/raygun") == "github"
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
 
-// permutations('a'); // ['a']
-// permutations('ab'); // ['ab', 'ba']
-// permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+function domainName(url) {
+    //your code here
+    let patt1 = new RegExp('http');
+    let patt2 = new RegExp('https');
+    let patt3 = new RegExp('www.');
+    let patt4
+
+
+
+    let string
+
+    if (patt1.test(url)) {
+        string = url.slice(7,)
+
+    }
+    console.log(string)
+}
+domainName('http://www.banana.com')
