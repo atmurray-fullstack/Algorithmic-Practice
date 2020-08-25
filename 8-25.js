b24084bcac5f7f190889a05d0a8fe416145e12ad
Andrew Murray <atmurray@bellsouth.net>
2020-08-25 16:15:33 -0400

    5 kyu codewars algo to move all zeros to the end off array and keep the order of other indices

--- 8-25.js
+++ 8-25.js
@@ -31,3 +31,27 @@
 // };
 // solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
 
+
+
+
+/////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+// var moveZeros = function (arr) {
+//     // TODO: Program me
+//     let newArr = [];
+//     newArr = arr.filter(el => {
+//         return el !== 0
+//     })
+//     for (let i = newArr.length; i < arr.length; i++) {
+//         newArr.push(0)
+//     }
+//     return newArr
+// }
+// moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])
