e7beb26db60e614d57c9d4459496fbbfad44d091
Andrew Murray <atmurray@bellsouth.net>
2020-09-02 21:36:35 -0400

    solved 5 kyu kata first nonrepeating character codewars

--- 8-30.js
+++ 8-30.js
@@ -75,3 +75,47 @@
 //     return result
 // }
 // add('1', '1')
+
+
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+// Write a function named first_non_repeating_letter that takes a string 
+// input, and returns the first character that is not repeated anywhere 
+// in the string.
+
+// For example, if given the input 'stress', the function should return 
+// 't', since the letter t only occurs once in the string, and occurs 
+// first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered 
+// the same character, but the function should return the correct case 
+// for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return an 
+// empty string ("") or None -- see sample tests.
+
+function firstNonRepeatingLetter(s) {
+    // Add your code here
+    let string = s.toLowerCase();
+    let index = null;
+    if (s.length === 0) {
+        return ''
+    }
+
+    for (let i = 0; i < string.length; i++) {
+        let stringArr = string.split('');
+        stringArr.splice(i, 1);
+        stringArr = stringArr.join('');
+        if (stringArr.indexOf(string[i]) === -1) {
+            index = i;
+            break;
+        }
+    }
+
+    return index === null ? '' : s[index];
+};
+
+firstNonRepeatingLetter('moonmon')
